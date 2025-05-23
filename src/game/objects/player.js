57a4d108ca8610b7034b525afe081ec59c03d22f
Sergio Guilherme <sergiopedroso_@hotmail.com>
2025-05-22 21:13:00 -0300

    mapa adiconado e direções do personagem

--- src/game/objects/player.js
+++ src/game/objects/player.js
@@ -1,42 +1,62 @@
 import Phaser from 'phaser';
 
 export const createPlayer = (scene) => {
-    const player = scene.physics.add.sprite(80,685, 'player_idle');
-    player.setScale(1.5,1.5);
-    createAnimations(scene);
-    return player;
+  const player = scene.physics.add.sprite(82, 80, 'player_idle');
+  player.setScale(1.2, 1.2);
+  // Ajuste hitbox (tamanho da colisão) e offset (deslocamento do hitbox)
+  player.setSize(20, 28);
+  player.setOffset(10, 12);
+  createAnimations(scene);
+  return player;
 };
 
 export const loadSprites = (scene) => {
-    scene.load.spritesheet('player_idle', 'Soldier-Idle.png', {
-        frameWidth: 100,
-        frameHeight: 100,
-        spacing: 0,
-    });
+  scene.load.spritesheet('player_idle', 'Cedric.png', {
+    frameWidth: 40,
+    frameHeight: 40,
+    spacing: 10,
+  });
 
-    scene.load.spritesheet('player_walk', 'Soldier-Walk.png', {
-        frameWidth: 100,
-        frameHeight: 100,
-        spacing: 0,
-    });
+  scene.load.spritesheet('player_walk', 'Cedric.png', {
+    frameWidth: 40,
+    frameHeight: 40,
+    spacing: 10,
+  });
 
-    scene.load.spritesheet('player_attack', 'Soldier-Attack01.png', {
-        frameWidth: 100,
-        frameHeight: 100,
-        spacing: 0,
-    });
+  scene.load.spritesheet('player_attack', 'Cedric.png', {
+    frameWidth: 40,
+    frameHeight: 40,
+    spacing: 0,
+  });
 };
 
 export const createAnimations = (scene) => {
   scene.anims.create({
-    key: 'player_idle',
-    frames: scene.anims.generateFrameNumbers('player_idle', {
-      start: 0,
-      end: 6
-    }),
-    frameRate: 7,
-    repeat: -1,
-    yoyo: true
+    key: 'idle_down',
+    frames: [ { key: 'player_idle', frame: 1 } ],
+    frameRate: 1,
+    repeat: 0
+  });
+
+  scene.anims.create({
+    key: 'idle_left',
+    frames: [ { key: 'player_idle', frame: 12 } ],
+    frameRate: 1,
+    repeat: 0
+  });
+
+  scene.anims.create({
+    key: 'idle_right',
+    frames: [ { key: 'player_idle', frame: 22 } ],
+    frameRate: 1,
+    repeat: 0
+  });
+
+  scene.anims.create({
+    key: 'idle_up',
+    frames: [ { key: 'player_idle', frame: 34 } ],
+    frameRate: 1,
+    repeat: 0
   });
 
   scene.anims.create({
@@ -58,4 +78,19 @@ export const createAnimations = (scene) => {
     frameRate: 10,
     repeat: 0
   });
+};
+
+export const updatePlayer = (player, cursors) => {
+  const speed = 100;
+  player.setVelocity(0);
+  if (cursors.left.isDown) {
+    player.anims.play('idle_left', true);
+  } else if (cursors.right.isDown) {
+    player.anims.play('idle_right', true);
+  } else if (cursors.up.isDown) {
+    player.anims.play('idle_up', true);
+  } else if (cursors.down.isDown) {
+    player.anims.play('idle_down', true);
+  } 
+
 };
--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -1,5 +1,7 @@
 import { Scene } from 'phaser';
 import { createPlayer } from '../objects/player';
+import { updatePlayer } from '../objects/player';
+
 
 export class Game extends Scene
 {
@@ -13,11 +15,28 @@ export class Game extends Scene
     {  
         const map = this.make.tilemap({ key: 'map' });
 
-        const tiledset = map.addTilesetImage('assets', 'tiles');
-        const groundLayer = map.createLayer('ground', tiledset, 0, 0);  
+        const tiledset = map.addTilesetImage('assests', 'tiles');
+        const fundo = map.createLayer('fundo', tiledset, 0, 0);  
+        const chao = map.createLayer('chao', tiledset, 0, 0);  
+        const parede = map.createLayer('parede', tiledset, 0, 0);
+        const detalhes = map.createLayer('detalhes', tiledset, 0, 0);
+        const itens = map.createLayer('itens', tiledset, 0, 0);
+        const pilar = map.createLayer('pilar', tiledset, 0, 0);
+        const cachoeira = map.createLayer('cachoeira', tiledset, 0, 0);
+        const detalhes2 = map.createLayer('detalhes / agua', tiledset, 0, 0);
+        const agua = map.createLayer('agua', tiledset, 0, 0);
 
         this.player = createPlayer(this);
 
-        this.player.anims.play('player_idle', true);
+        parede.setCollisionByExclusion([-1]);
+
+        this.physics.add.collider(this.player, parede);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    }
+
+    update() {
+        updatePlayer(this.player, this.cursors);
     }
 }
